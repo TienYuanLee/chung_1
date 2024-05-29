4f2f064752bf50dfee4178a08ec46e905814362b
601042 <167844614+601042@users.noreply.github.com>
2024-05-29 16:47:42 +0800

    Update message.py

--- message.py
+++ message.py
@@ -3,52 +3,6 @@ from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
-#ImagemapSendMessage(組圖訊息)
-def imagemap_message():
-    message = ImagemapSendMessage(
-        base_url="https://i.imgur.com/BfTFVDN.jpg",
-        alt_text='最新的合作廠商有誰呢？',
-        base_size=BaseSize(height=2000, width=2000),
-        actions=[
-            URIImagemapAction(
-                #家樂福
-                link_uri="https://tw.shop.com/search/%E5%AE%B6%E6%A8%82%E7%A6%8F",
-                area=ImagemapArea(
-                    x=0, y=0, width=1000, height=1000
-                )
-            ),
-            URIImagemapAction(
-                #生活市集
-                link_uri="https://tw.shop.com/search/%E7%94%9F%E6%B4%BB%E5%B8%82%E9%9B%86",
-                area=ImagemapArea(
-                    x=1000, y=0, width=1000, height=1000
-                )
-            ),
-            URIImagemapAction(
-                #阿瘦皮鞋
-                link_uri="https://tw.shop.com/search/%E9%98%BF%E7%98%A6%E7%9A%AE%E9%9E%8B",
-                area=ImagemapArea(
-                    x=0, y=1000, width=1000, height=1000
-                )
-            ),
-            URIImagemapAction(
-                #塔吉特千層蛋糕
-                link_uri="https://tw.shop.com/search/%E5%A1%94%E5%90%89%E7%89%B9",
-                area=ImagemapArea(
-                    x=1000, y=1000, width=1000, height=500
-                )
-            ),
-            URIImagemapAction(
-                #亞尼克生乳捲
-                link_uri="https://tw.shop.com/search/%E4%BA%9E%E5%B0%BC%E5%85%8B",
-                area=ImagemapArea(
-                    x=1000, y=1500, width=1000, height=500
-                )
-            )
-        ]
-    )
-    return message
-
 #TemplateSendMessage - ButtonsTemplate (按鈕介面訊息)
 def buttons_message():
     message = TemplateSendMessage(
@@ -83,17 +37,17 @@ def buttons_message():
 def Confirm_Template():
 
     message = TemplateSendMessage(
-        alt_text='是否註冊成為會員？',
+        alt_text='想吃甚麼呢?',
         template=ConfirmTemplate(
-            text="是否註冊成為會員？",
+            text="想吃神麼呢",
             actions=[
                 PostbackTemplateAction(
-                    label="馬上註冊",
+                    label="查詢食譜",
                     text="現在、立刻、馬上",
                     data="會員註冊"
                 ),
                 MessageTemplateAction(
-                    label="查詢其他功能",
+                    label="新增食材",
                     text="查詢其他功能"
                 )
             ]
